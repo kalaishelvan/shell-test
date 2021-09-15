72d1a9248962a68c006b3061cab02566d885bcda
kalaishelvan1018@gmail.com <root@DESKTOP-ERTO0CM.localdomain>
2021-09-15 22:22:41 +0530

    new test file

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "select a number from 10 to 20  :  "
+read VAL
+
+if [ $VAL -gt 20 ]
+then
+echo "The number is greater than 20"
+else
+echo "The number is lesser than 20"
+fi
+if [ $VAL -eq 10 ]
+then
+echo "The value is equal to 10"
+fi
