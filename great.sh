3c0882d6e801e9ea24e482a6ae682470d1c7626f
kalaishelvan <58733846+kalaishelvan@users.noreply.github.com>
2021-09-15 21:17:09 +0530

    Changed value to 19

--- great.sh
+++ great.sh
@@ -5,7 +5,7 @@ read VAL
 
 if [[  $VAL -gt 10 ]]
 then
-echo "The variable is greater than 10"
+echo "The variable is greater than 19"
 else
-echo "The variable is lesser than 10"
+echo "The variable is lesser than 19"
 fi
